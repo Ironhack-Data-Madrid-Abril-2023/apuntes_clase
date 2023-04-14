a3c34e7ee5a06da0f4746a39d089f9bcfb235386
iudh <yeho@mail.com>
2023-04-14 08:22:26 +0200

    funcional - oop

---
+++ semana_1/funciones.py
@@ -0,0 +1,33 @@
+def sumar(a, b):
+    """
+    Hola, estoy en el archivo de python
+    
+    """
+    return a+b
+
+
+
+def restar(a, b):
+    """
+    Hola, estoy en el archivo de python
+    
+    """
+    return a-b
+
+
+
+def multi(a, b):
+    """
+    Hola, estoy en el archivo de python
+    
+    """
+    return a*b
+
+
+
+def divi(a, b):
+    """
+    Hola, estoy en el archivo de python
+    
+    """
+    return a/b
---
+++ semana_1/src/funciones.py
@@ -0,0 +1,16 @@
+def sumar(a, b):
+    """
+    Funcion para sumar
+    """
+    return a+b
+
+def restar(a, b):
+    return a-b
+
+def multiplicar(a, b):
+    return a*b
+
+def dividir(a, b):
+    return a//b
+
+
